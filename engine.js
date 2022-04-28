850065b072443b068ec104c4bfe5deaa70138f38
razha123 <razha9046123@gmail.com>
2022-04-27 19:47:01 -0700

    update

---
+++ engine.js
@@ -0,0 +1,76 @@
+var i = true;
+function click1() {
+    if (i == true) {
+    document.getElementById('button-1').style.backgroundColor = "#fc7613";
+    i = false;
+    } else {
+        document.getElementById('button-1').style.backgroundColor = "#30393f";
+        i =true;
+    }
+}
+
+var o = true;
+function click2() {
+    if (o == true) {
+    document.getElementById('button-2').style.backgroundColor = "#fc7613";
+    o = false;
+    } else {
+        document.getElementById('button-2').style.backgroundColor = "#30393f";
+        o =true;
+    }
+}
+
+var p = true;
+function click3() {
+    if (p == true) {
+    document.getElementById('button-3').style.backgroundColor = "#fc7613";
+    p = false;
+    } else {
+        document.getElementById('button-3').style.backgroundColor = "#30393f";
+        p =true;
+    }
+}
+
+var q = true;
+function click4() {
+    if (q == true) {
+    document.getElementById('button-4').style.backgroundColor = "#fc7613";
+    q = false;
+    } else {
+        document.getElementById('button-4').style.backgroundColor = "#30393f";
+        q =true;
+    }
+}
+
+var r = true;
+function click5() {
+    if (r == true) {
+    document.getElementById('button-5').style.backgroundColor = "#fc7613";
+    r = false;
+    } else {
+        document.getElementById('button-5').style.backgroundColor = "#30393f";
+        r =true;
+    }
+}
+
+function clicked() {
+    document.getElementById('hilang1').style.display = 'none';
+    document.getElementById('tampil').style.display = "block";
+    var get = 0;
+    if (i == false) {
+        get++;
+    }
+    if (o == false) {
+        get++;
+    }
+    if (p == false) {
+        get++;
+    }
+    if (q == false) {
+        get++;
+    }
+    if (r == false) {
+        get++;
+    }
+    document.getElementById('score').innerHTML = get;
+}
